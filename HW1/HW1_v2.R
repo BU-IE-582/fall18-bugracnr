ce88400c0005855a0fca7f6dea1466045623db25
Alim Buğra Çınar <abugracinar@gmail.com>
2018-10-17 20:49:50 +0300

    Task 2
    Time effect is analyzed.

--- HW1/HW1_v2.R
+++ HW1/HW1_v2.R
@@ -55,10 +55,10 @@ odds_ov_un=odds[betType=='ou' & totalhandicap=='2.5']
 #order data in ascending date & find initial and final matches
 odds_ov_un=odds_ov_un[order(matchId, oddtype,bookmaker,date)]
 
-odds_ov_un_initial=odds_ov_un[,list(selected_odd=odd[1]),
+odds_ov_un_initial=odds_ov_un[,list(start_odd=odd[1]),
                               by=list(matchId,oddtype,bookmaker)]
 
-odds_ov_un_final=odds_ov_un[,list(selected_odd=odd[.N]),
+odds_ov_un_final=odds_ov_un[,list(final_odd=odd[.N]),
                             by=list(matchId,oddtype,bookmaker)]
 
 
@@ -110,11 +110,11 @@ pinnacle_initial_summary=pinnacle_initial[,list(empirical_over=mean(IsOver),
 pinnacle_final_summary=pinnacle_final[,list(empirical_over=mean(IsOver),
                                             probabilistic_over=mean(probOver),.N),
                                       by=list(odd_cut_over)]
-
+?plot
 #total aggreagate analysis
-plot(pinnacle_initial_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "blue", pch = 2)
-points(pinnacle_final_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "purple", pch = 5)
-abline(0,1,col='red')
+plot(pinnacle_initial_summary[,list(empirical_over,probabilistic_over)],cex=1, col = "blue", pch = 2, xlab = "Empirical Over", ylab = "Probabilistic Over", main = "Comparison of Initial and Final Probabilities")
+points(pinnacle_final_summary[,list(empirical_over,probabilistic_over)],cex=1, col = "red", pch = 5)
+abline(0,1,col='green')
 
 ####Task 1-b
 
@@ -141,7 +141,8 @@ plotYears <- function(initial_data, final_data, minprob, maxprob, probCut)
   minX <- min(initial_data[odd_cut_over == probCut,list(empirical_over)],final_data[odd_cut_over == probCut,list(empirical_over)])
   maxX <- 0.1 + max(initial_data[odd_cut_over == probCut,list(empirical_over)],final_data[odd_cut_over == probCut,list(empirical_over)])
 
-  plot(initial_data[odd_cut_over == probCut,list(empirical_over,probabilistic_over)], ylim = c(minY, maxY), xlim = c(minX, maxX),cex=2, pch = 2, col= initial_data[odd_cut_over == probCut]$Year - 2010)
+  plot(initial_data[odd_cut_over == probCut,list(empirical_over,probabilistic_over)], ylim = c(minY, maxY), xlim = c(minX, maxX),cex=2, pch = 2, 
+       xlab = "Empirical Over Values", ylab = "Probabilistic Over Values", main = "Comparison of Initial and Final Values by Years", col= initial_data[odd_cut_over == probCut]$Year - 2010)
   points(final_data[odd_cut_over == probCut,list(empirical_over,probabilistic_over)],cex=2, pch = 5, col= final_data[odd_cut_over == probCut]$Year - 2010)
   abline(0,1,col='red')
   abline(v = minprob)
@@ -158,7 +159,7 @@ plotSeries <- function(initial_data, final_data, minprob, maxprob)
                 final_data[odd_cut_over == probCut,list(empirical_over)])
   maxVal <- max(initial_data[odd_cut_over == probCut,list(empirical_over)],
                 final_data[odd_cut_over == probCut,list(empirical_over)])
-  plot(initial_data[odd_cut_over == probCut,list(Year,empirical_over)], 
+  plot(initial_data[odd_cut_over == probCut,list(Year,empirical_over)], ylab = "Empirical Over", main = "Change in Probabilities Over Time",
        ylim = c(minVal,maxVal), cex=2, pch = 3, col= initial_data[odd_cut_over == probCut]$Year - 2010)
   points(final_data[odd_cut_over == probCut,list(Year,empirical_over)],
          cex=2, pch = 4, col= initial_data[odd_cut_over == probCut]$Year - 2010)
@@ -189,7 +190,7 @@ betsafe_final_summary=betsafe_final[,list(empirical_over=mean(IsOver),
                                       by=list(odd_cut_over)]
 
 #total aggreagate analysis
-plot(betsafe_initial_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "blue", pch = 2)
+plot(betsafe_initial_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "blue", pch = 2,xlab = "Empirical Over", ylab = "Probabilistic Over", main = "Comparison of Initial and Final Probabilities")
 points(betsafe_final_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "purple", pch = 5)
 abline(0,1,col='red')
 
@@ -214,6 +215,129 @@ plotYears(betsafe_initial_yearly_analysis,betsafe_final_yearly_analysis,0.5,0.55
 plotSeries(betsafe_initial_yearly_analysis,betsafe_final_yearly_analysis,0.5,0.55)
 
 
+######Sportingbet
+
+sportingbet_initial <- initial_merged_data[bookmaker == "Sportingbet"]
+sportingbet_final <- final_merged_data[bookmaker == "Sportingbet"]
+
+
+sportingbet_initial_summary=sportingbet_initial[,list(empirical_over=mean(IsOver),
+                                              probabilistic_over=mean(probOver),.N),
+                                        by=list(odd_cut_over)]
+
+
+sportingbet_final_summary=sportingbet_final[,list(empirical_over=mean(IsOver),
+                                          probabilistic_over=mean(probOver),.N),
+                                    by=list(odd_cut_over)]
+
+#total aggreagate analysis
+plot(sportingbet_initial_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "blue", pch = 2, xlab = "Empirical Over", ylab = "Probabilistic Over", main = "Comparison of Initial and Final Probabilities")
+points(sportingbet_final_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "purple", pch = 5)
+abline(0,1,col='red')
+
+####Task 1-b
+
+#analysis by years
+sportingbet_final_yearly_analysis=sportingbet_final[,list(empirical_over=mean(IsOver),
+                                                  probabilistic_over=mean(probOver),.N),
+                                            by=list(Year,odd_cut_over)]
+
+sportingbet_final_yearly_analysis=sportingbet_final_yearly_analysis[order(Year)]
+
+#analysis by years
+sportingbet_initial_yearly_analysis=sportingbet_initial[,list(empirical_over=mean(IsOver),
+                                                      probabilistic_over=mean(probOver),.N),
+                                                by=list(Year,odd_cut_over)]
+
+sportingbet_initial_yearly_analysis=sportingbet_initial_yearly_analysis[order(Year)]
+
+
+plotYears(sportingbet_initial_yearly_analysis,sportingbet_final_yearly_analysis,0.5,0.55)
+plotSeries(sportingbet_initial_yearly_analysis,sportingbet_final_yearly_analysis,0.5,0.55)
+
+
+#####Tipico
+
+tipico_initial <- initial_merged_data[bookmaker == "Tipico"]
+tipico_final <- final_merged_data[bookmaker == "Tipico"]
+
+
+tipico_initial_summary=tipico_initial[,list(empirical_over=mean(IsOver),
+                                                      probabilistic_over=mean(probOver),.N),
+                                                by=list(odd_cut_over)]
+
+
+tipico_final_summary=tipico_final[,list(empirical_over=mean(IsOver),
+                                                  probabilistic_over=mean(probOver),.N),
+                                            by=list(odd_cut_over)]
+
+#total aggreagate analysis
+plot(tipico_initial_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "blue", pch = 2, xlab = "Empirical Over", ylab = "Probabilistic Over", main = "Comparison of Initial and Final Probabilities")
+points(tipico_final_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "purple", pch = 5)
+abline(0,1,col='red')
+
+####Task 1-b
+
+#analysis by years
+tipico_final_yearly_analysis=tipico_final[,list(empirical_over=mean(IsOver),
+                                                          probabilistic_over=mean(probOver),.N),
+                                                    by=list(Year,odd_cut_over)]
+
+tipico_final_yearly_analysis=tipico_final_yearly_analysis[order(Year)]
+
+#analysis by years
+tipico_initial_yearly_analysis=tipico_initial[,list(empirical_over=mean(IsOver),
+                                                              probabilistic_over=mean(probOver),.N),
+                                                        by=list(Year,odd_cut_over)]
+
+tipico_initial_yearly_analysis=tipico_initial_yearly_analysis[order(Year)]
+
+
+plotYears(tipico_initial_yearly_analysis,tipico_final_yearly_analysis,0.5,0.55)
+plotSeries(tipico_initial_yearly_analysis,tipico_final_yearly_analysis,0.5,0.55)
+
+#####Interwetten
+
+#####interwetten
+
+interwetten_initial <- initial_merged_data[bookmaker == "Interwetten"]
+interwetten_final <- final_merged_data[bookmaker == "Interwetten"]
+
+
+interwetten_initial_summary=interwetten_initial[,list(empirical_over=mean(IsOver),
+                                            probabilistic_over=mean(probOver),.N),
+                                      by=list(odd_cut_over)]
+
+
+interwetten_final_summary=interwetten_final[,list(empirical_over=mean(IsOver),
+                                        probabilistic_over=mean(probOver),.N),
+                                  by=list(odd_cut_over)]
+
+#total aggreagate analysis
+plot(interwetten_initial_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "blue", pch = 2, xlab = "Empirical Over", ylab = "Probabilistic Over", main = "Comparison of Initial and Final Probabilities")
+points(interwetten_final_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "purple", pch = 5)
+abline(0,1,col='red')
+
+####Task 1-b
+
+#analysis by years
+interwetten_final_yearly_analysis=interwetten_final[,list(empirical_over=mean(IsOver),
+                                                probabilistic_over=mean(probOver),.N),
+                                          by=list(Year,odd_cut_over)]
+
+interwetten_final_yearly_analysis=interwetten_final_yearly_analysis[order(Year)]
+
+#analysis by years
+interwetten_initial_yearly_analysis=interwetten_initial[,list(empirical_over=mean(IsOver),
+                                                    probabilistic_over=mean(probOver),.N),
+                                              by=list(Year,odd_cut_over)]
+
+interwetten_initial_yearly_analysis=interwetten_initial_yearly_analysis[order(Year)]
+
+
+plotYears(interwetten_initial_yearly_analysis,interwetten_final_yearly_analysis,0.5,0.55)
+plotSeries(interwetten_initial_yearly_analysis,interwetten_final_yearly_analysis,0.5,0.55)
+
 
 ############### 1x2 Data will be worked on under this part
 
@@ -229,6 +353,35 @@ odds_1x2_initial=odds_1x2[,list(start_odd=odd[1]),
 
 odds_1x2_final=odds_1x2[,list(final_odd=odd[.N]),
                             by=list(matchId,oddtype,bookmaker)]
+odds1 <- odds_1x2[oddtype == "odd1"]
+
+
+#### effect of time on number of changes #####
+number_of_changes <- odds1[,list(changeNum = length(odd)), by = list(matchId, oddtype, bookmaker)]
+number_of_changes <- merge(number_of_changes, matches[,c("matchId", "Year")], by = "matchId")
+number_of_changes[complete.cases(number_of_changes)]
+
+hist(number_of_changes$changeNum)
+
+number_of_changes <- number_of_changes[changeNum > 2]
+hist(number_of_changes$changeNum)
+
+large_changes <- number_of_changes[changeNum > 300]
+hist(large_changes$changeNum)
+
+hist_data =number_of_changes[,list(totalchanges=sum(changeNum)), by=list(Year)]
+plot(hist_data)
+
+mean_data <- number_of_changes[,list(mean=mean(changeNum)), by=list(Year)]
+plot(mean_data)
+
+
+max(number_of_changes$changeNum)
+number_of_changes[changeNum == 812]
+matches[matchId == "QHU3UeaL"]
+#### number of changes -> years
+
+
 
 #merged_1x2 <- merge(odds_1x2_initial, odds_1x2_final, by = c("matchId", "oddtype", "bookmaker"))
 
@@ -286,18 +439,18 @@ final_probx_hist <- hist(melted_final_1x2$probx)
 
 #histograms
 #prob1
-plot(initial_prob1_hist, col = rgb(1,0,0,1/4), xlim = c(0,1))
+plot(initial_prob1_hist, col = rgb(1,0,0,1/4), xlim = c(0,1), xlab =  "Initial and Final Bins", main = "Initial and Final 1 Distributions")
 plot(final_prob1_hist, col = rgb(0,0,1,1/4), xlim = c(0,1), add = TRUE)
 lines(initial_prob1_hist$mids, initial_prob1_hist$counts,  col = "red", type = "o")
 lines(final_prob1_hist$mids, final_prob1_hist$counts, col = "blue", type = "o")
 
 
-plot(initial_prob2_hist, col = rgb(1,0,0,1/4), xlim = c(0,1))
+plot(initial_prob2_hist, col = rgb(1,0,0,1/4), xlim = c(0,1), xlab =  "Initial and Final Bins", main = "Initial and Final 2 Distributions")
 plot(final_prob2_hist, col = rgb(0,0,1,1/4), xlim = c(0,1), add = TRUE)
 lines(initial_prob2_hist$mids, initial_prob2_hist$counts,  col = "red", type = "o")
 lines(final_prob2_hist$mids, final_prob2_hist$counts, col = "blue", type = "o")
 
-plot(initial_probx_hist, col = rgb(1,0,0,1/4), xlim = c(0,1))
+plot(initial_probx_hist, col = rgb(1,0,0,1/4), xlim = c(0,1), xlab =  "Initial and Final Bins", main = "Initial and Final X Distributions")
 plot(final_probx_hist, col = rgb(0,0,1,1/4), xlim = c(0,1), add = TRUE)
 lines(initial_probx_hist$mids, initial_probx_hist$counts,  col = "red", type = "o")
 lines(final_probx_hist$mids, final_probx_hist$counts, col = "blue", type = "o")
@@ -386,4 +539,6 @@ abline(0,1,col='red')
 plot(betsafe_pX_1x2_initial_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "blue", pch = 2)
 points(betsafe_pX_1x2_final_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "purple", pch = 5)
 abline(0,1,col='red')
+
+
 
