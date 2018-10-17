da62d60a36397c8581845566f7d802056f8869c6
Alim Buğra Çınar <abugracinar@gmail.com>
2018-10-17 19:16:29 +0300

    Removed initial and final prep functions
    The functionalities are merged into single statement. They become obsolete, therefore removed.

--- HW1/HW1_v2.R
+++ HW1/HW1_v2.R
@@ -56,66 +56,51 @@ odds_ov_un=odds[betType=='ou' & totalhandicap=='2.5']
 #order data in ascending date & find initial and final matches
 odds_ov_un=odds_ov_un[order(matchId, oddtype,bookmaker,date)]
 
-odds_ov_un_initial=odds_ov_un[,list(start_odd=odd[1]),
+odds_ov_un_initial=odds_ov_un[,list(selected_odd=odd[1]),
                               by=list(matchId,oddtype,bookmaker)]
 
-odds_ov_un_final=odds_ov_un[,list(final_odd=odd[.N]),
+odds_ov_un_final=odds_ov_un[,list(selected_odd=odd[.N]),
                             by=list(matchId,oddtype,bookmaker)]
 
 
-initialDataPrep <- function(bookmaker_name)
-{
- 
-  wide_data <- dcast(odds_ov_un_initial[bookmaker==bookmaker_name],
-                     matchId~oddtype,
-                     value.var="start_odd")
-  merged_data <- merge(matches[,c("matchId", "IsOver","Year")],wide_data,by='matchId')
-  merged_data[,probOver:=1/over]
-  merged_data[,probUnder:=1/under]
-  
-  merged_data[,totalProb:=probOver+probUnder]
-  
-  merged_data[,probOver:=probOver/totalProb]
-  merged_data[,probUnder:=probUnder/totalProb]
-  
-  
-  
-  merged_data=merged_data[complete.cases(merged_data)]
-  merged_data[,totalProb:=NULL]
-  
-  cutpoints=seq(0,1,0.05)
-  merged_data[,odd_cut_over:=cut(probOver,cutpoints)]
-}
+melted_ov_un_initial <- dcast(odds_ov_un_initial,
+                              matchId + bookmaker ~oddtype,
+                              value.var="start_odd")
 
-finalDataPrep <- function(bookmaker_name)
-{
-  
-  wide_data <- dcast(odds_ov_un_final[bookmaker==bookmaker_name],
-                     matchId~oddtype,
+cutpoints=seq(0,1,0.05)
+
+
+initial_merged_data <- merge(matches[,c("matchId", "IsOver","Year")],melted_ov_un_initial,by='matchId')
+initial_merged_data[,probOver:=1/over]
+initial_merged_data[,probUnder:=1/under]
+initial_merged_data[,totalProb:=probOver+probUnder]
+initial_merged_data[,probOver:=probOver/totalProb]
+initial_merged_data[,probUnder:=probUnder/totalProb]
+initial_merged_data=initial_merged_data[complete.cases(initial_merged_data)]
+initial_merged_data[,totalProb:=NULL]
+initial_merged_data[,odd_cut_over:=cut(probOver,cutpoints)]
+
+
+
+melted_ov_un_final <- dcast(odds_ov_un_final,
+                     matchId + bookmaker ~oddtype,
                      value.var="final_odd")
-  merged_data <- merge(matches[,c("matchId", "IsOver","Year")],wide_data,by='matchId')
-  merged_data[,probOver:=1/over]
-  merged_data[,probUnder:=1/under]
-  
-  merged_data[,totalProb:=probOver+probUnder]
-  
-  merged_data[,probOver:=probOver/totalProb]
-  merged_data[,probUnder:=probUnder/totalProb]
-  
-  
-  
-  merged_data=merged_data[complete.cases(merged_data)]
-  merged_data[,totalProb:=NULL]
-  
-  cutpoints=seq(0,1,0.05)
-  merged_data[,odd_cut_over:=cut(probOver,cutpoints)]
-}
+
+  final_merged_data <- merge(matches[,c("matchId", "IsOver","Year")],melted_ov_un_final,by='matchId')
+  final_merged_data[,probOver:=1/over]
+  final_merged_data[,probUnder:=1/under]
+  final_merged_data[,totalProb:=probOver+probUnder]
+  final_merged_data[,probOver:=probOver/totalProb]
+  final_merged_data[,probUnder:=probUnder/totalProb]
+  final_merged_data=final_merged_data[complete.cases(final_merged_data)]
+  final_merged_data[,totalProb:=NULL]
+  final_merged_data[,odd_cut_over:=cut(probOver,cutpoints)]
+
 
 #pinnacle is the first bookmaker that I choose, first initial odds analysis
 
-pinnacle_initial <- initialDataPrep("Pinnacle")
-pinnacle_final <- finalDataPrep("Pinnacle")
-
+pinnacle_initial <- initial_merged_data[bookmaker == "Pinnacle"]
+pinnacle_final <- final_merged_data[bookmaker == "Pinnacle"]
 
 #initial aggregate analysis
 pinnacle_initial_summary=pinnacle_initial[,list(empirical_over=mean(IsOver),
@@ -152,25 +137,25 @@ pinnacle_initial_yearly_analysis=pinnacle_initial_yearly_analysis[order(Year)]
 
 plotYears <- function(initial_data, final_data, minprob, maxprob, probCut)
 {
- minY <- min(initial_data[odd_cut_over == probCut,list(probabilistic_over)],final_data[odd_cut_over == probCut,list(probabilistic_over)])
-   maxY <- max(initial_data[odd_cut_over == probCut,list(probabilistic_over)],final_data[odd_cut_over == probCut,list(probabilistic_over)])
+  probCut <- paste("(", minprob, ",", maxprob, "]", sep = "")
+  minY <- min(initial_data[odd_cut_over == probCut,list(probabilistic_over)],final_data[odd_cut_over == probCut,list(probabilistic_over)])
+  maxY <- max(initial_data[odd_cut_over == probCut,list(probabilistic_over)],final_data[odd_cut_over == probCut,list(probabilistic_over)])
+  minX <- min(initial_data[odd_cut_over == probCut,list(empirical_over)],final_data[odd_cut_over == probCut,list(empirical_over)])
+  maxX <- 0.1 + max(initial_data[odd_cut_over == probCut,list(empirical_over)],final_data[odd_cut_over == probCut,list(empirical_over)])
 
-   minX <- min(initial_data[odd_cut_over == probCut,list(empirical_over)],final_data[odd_cut_over == probCut,list(empirical_over)])
-
-   maxX <- 0.1 + max(initial_data[odd_cut_over == probCut,list(empirical_over)],final_data[odd_cut_over == probCut,list(empirical_over)])
-plot(initial_data[odd_cut_over == probCut,list(empirical_over,probabilistic_over)], ylim = c(minY, maxY), xlim = c(minX, maxX),cex=2, pch = 2, col= initial_data[odd_cut_over == probCut]$Year - 2010)
-points(final_data[odd_cut_over == probCut,list(empirical_over,probabilistic_over)],cex=2, pch = 5, col= final_data[odd_cut_over == probCut]$Year - 2010)
-abline(0,1,col='red')
-abline(v = minprob)
-abline(v = maxprob)
-legend("topright",cex = 0.55, legend = initial_data[odd_cut_over == probCut]$Year, 
+  plot(initial_data[odd_cut_over == probCut,list(empirical_over,probabilistic_over)], ylim = c(minY, maxY), xlim = c(minX, maxX),cex=2, pch = 2, col= initial_data[odd_cut_over == probCut]$Year - 2010)
+  points(final_data[odd_cut_over == probCut,list(empirical_over,probabilistic_over)],cex=2, pch = 5, col= final_data[odd_cut_over == probCut]$Year - 2010)
+  abline(0,1,col='red')
+  abline(v = minprob)
+  abline(v = maxprob)
+  legend("topright",cex = 0.55, legend = initial_data[odd_cut_over == probCut]$Year, 
        fill = initial_data[odd_cut_over == probCut]$Year - 2010)
 }
 
-#time series data
-### Special Plot Function
-plotSeries <- function(initial_data, final_data, minprob, maxprob, probCut)
+
+plotSeries <- function(initial_data, final_data, minprob, maxprob)
 {
+  probCut <- paste("(", minprob, ",", maxprob, "]", sep = "")
   minVal <- min(initial_data[odd_cut_over == probCut,list(empirical_over)],
                 final_data[odd_cut_over == probCut,list(empirical_over)])
   maxVal <- max(initial_data[odd_cut_over == probCut,list(empirical_over)],
@@ -186,50 +171,7 @@ plotSeries <- function(initial_data, final_data, minprob, maxprob, probCut)
 }
 
 
-plotYears(pinnacle_initial_yearly_analysis,pinnacle_final_yearly_analysis, 0.45, 0.5, "(0.45,0.5]")
 
-
-#aggregate analysis
-betsafe_initial_summary=betsafe_initial[,list(empirical_over=mean(IsOver),
-                                                probabilistic_over=mean(probOver),.N),
-                                          by=list(odd_cut_over)]
-
-
-plot(betsafe_initial_summary[,list(empirical_over,probabilistic_over)],cex=4)
-abline(0,1,col='red')
-
-betsafe_initial <- initialDataPrep("Betsafe")
-betsafe_final <- finalDataPrep("Betsafe")
-
-#analysis by years
-betsafe_initial_yearly_analysis=betsafe_initial[,list(empirical_over=mean(IsOver),
-                                                        probabilistic_over=mean(probOver),.N),
-                                                  by=list(Year,odd_cut_over)]
-
-betsafe_initial_yearly_analysis=betsafe_initial_yearly_analysis[order(Year)]
-
-plot(betsafe_initial_yearly_analysis[,list(empirical_over,probabilistic_over)],cex=1, col= betsafe_initial_yearly_analysis$Year - 2009)
-abline(0,1,col='red')
-
-#betsafe final odds analysis
-
-betsafe_final_summary=betsafe_final[,list(empirical_over=mean(IsOver),
-                                            probabilistic_over=mean(probOver),.N),
-                                      by=list(odd_cut_over)]
-
-
-plot(betsafe_final_summary[,list(empirical_over,probabilistic_over)],cex=4)
-abline(0,1,col='red')
-
-#analysis by years
-betsafe_final_yearly_analysis=betsafe_final[,list(empirical_over=mean(IsOver),
-                                                    probabilistic_over=mean(probOver),.N),
-                                              by=list(Year,odd_cut_over)]
-
-betsafe_final_yearly_analysis=betsafe_final_yearly_analysis[order(Year)]
-
-plot(betsafe_final_yearly_analysis[,list(empirical_over,probabilistic_over)],cex=4)
-abline(0,1,col='red')
 
 ############### 1x2 Data will be worked on under this part
 str(odds)
@@ -251,6 +193,7 @@ odds_1x2_final=odds_1x2[,list(final_odd=odd[.N]),
 
 #merged_1x2 <- merge(odds_1x2_initial, odds_1x2_final, by = c("matchId", "oddtype", "bookmaker"))
 
+
 ###melted all data
 melted_initial_1x2 <- dcast(odds_1x2_initial,
                            matchId + bookmaker ~oddtype,
@@ -266,10 +209,12 @@ melted_initial_1x2[,prob2 := prob2/totalprob]
 melted_initial_1x2[,probx := probx/totalprob]
 melted_initial_1x2[,totalprob := NULL]
 melted_initial_1x2[complete.cases(melted_initial_1x2)]
+melted_initial_1x2[,prob1_cut:=cut(prob1,cutpoints)]
+melted_initial_1x2[,prob2_cut:=cut(prob2,cutpoints)]
+melted_initial_1x2[,probx_cut:=cut(probx,cutpoints)]
+melted_initial_1x2 <- merge(melted_initial_1x2,matches[,c("matchId", "Is1","Is2","IsX")],by='matchId')
 
 
-summary(melted_initial_1x2)
-summary(melted_final_1x2)
 
 melted_final_1x2 <- dcast(odds_1x2_final,
       matchId + bookmaker ~oddtype,
@@ -285,14 +230,23 @@ melted_final_1x2[,prob2 := prob2/totalprob]
 melted_final_1x2[,probx := probx/totalprob]
 melted_final_1x2[,totalprob := NULL]
 melted_final_1x2[complete.cases(melted_final_1x2)]
+melted_final_1x2[,prob1_cut:=cut(prob1,cutpoints)]
+melted_final_1x2[,prob2_cut:=cut(prob2,cutpoints)]
+melted_final_1x2[,probx_cut:=cut(probx,cutpoints)]
+melted_final_1x2 <- merge(melted_final_1x2,matches[,c("matchId", "Is1","Is2","IsX")],by='matchId')
+
 
 initial_prob1_hist <- hist(melted_initial_1x2$prob1)
 initial_prob2_hist <- hist(melted_initial_1x2$prob2)
 initial_probx_hist <- hist(melted_initial_1x2$probx)
+
 final_prob1_hist <- hist(melted_final_1x2$prob1)
 final_prob2_hist <- hist(melted_final_1x2$prob2)
 final_probx_hist <- hist(melted_final_1x2$probx)
 
+
+#histograms
+#prob1
 plot(initial_prob1_hist, col = rgb(1,0,0,1/4), xlim = c(0,1))
 plot(final_prob1_hist, col = rgb(0,0,1,1/4), xlim = c(0,1), add = TRUE)
 lines(initial_prob1_hist$mids, initial_prob1_hist$counts,  col = "red", type = "o")
@@ -310,30 +264,15 @@ lines(initial_probx_hist$mids, initial_probx_hist$counts,  col = "red", type = "
 lines(final_probx_hist$mids, final_probx_hist$counts, col = "blue", type = "o")
 
 
-cutpoints=seq(0,1,0.05)
-melted_initial_1x2[,prob1_cut:=cut(prob1,cutpoints)]
-melted_initial_1x2[,prob2_cut:=cut(prob2,cutpoints)]
-melted_initial_1x2[,probx_cut:=cut(probx,cutpoints)]
+pinnacle_1x2_initial <- melted_initial_1x2[bookmaker=="Pinnacle"]
+pinnacle_1x2_final <- melted_final_1x2[bookmaker=="Pinnacle"]
 
-melted_final_1x2[,prob1_cut:=cut(prob1,cutpoints)]
-melted_final_1x2[,prob2_cut:=cut(prob2,cutpoints)]
-melted_final_1x2[,probx_cut:=cut(probx,cutpoints)]
-
-melted_final_1x2a <- merge(melted_final_1x2,matches[,c("matchId", "Is1","Is2","IsX")],by='matchId')
-
-pinnacle_1x2_initial <- melted_initial_1x2a[bookmaker=="Pinnacle"]
-pinnacle__1x2_initial_summary=pinnacle_1x2_initial[,list(empirical_over=mean(Is1),
+pinnacle_p1_1x2_initial_summary=pinnacle_1x2_initial[,list(empirical_over=mean(Is1),
                                                 probabilistic_over=mean(prob1),.N),
                                           by=list(prob1_cut)]
-pinnacle_1x2_final <- melted_final_1x2a[bookmaker=="Pinnacle"]
-pinnacle__1x2_final_summary=pinnacle_1x2_final[,list(empirical_over=mean(Is1),
+pinnacle_p1_1x2_final_summary=pinnacle_1x2_final[,list(empirical_over=mean(Is1),
                                                          probabilistic_over=mean(prob1),.N),
                                                    by=list(prob1_cut)]
-
-plot(pinnacle__1x2_initial_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "blue", pch = 2)
-points(pinnacle__1x2_final_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "purple", pch = 5)
-abline(0,1,col='red')
-
 
 
 pinnacle_p2_1x2_initial_summary=pinnacle_1x2_initial[,list(empirical_over=mean(Is2),
@@ -343,17 +282,23 @@ pinnacle_p2_1x2_final_summary=pinnacle_1x2_final[,list(empirical_over=mean(Is2),
                                                      probabilistic_over=mean(prob2),.N),
                                                by=list(prob2_cut)]
 
-plot(pinnacle_p2_1x2_initial_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "blue", pch = 2)
-points(pinnacle_p2_1x2_final_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "purple", pch = 5)
-abline(0,1,col='red')
-
-
 pinnacle_pX_1x2_initial_summary=pinnacle_1x2_initial[,list(empirical_over=mean(IsX),
                                                            probabilistic_over=mean(probx),.N),
                                                      by=list(probx_cut)]
 pinnacle_pX_1x2_final_summary=pinnacle_1x2_final[,list(empirical_over=mean(IsX),
                                                        probabilistic_over=mean(probx),.N),
                                                  by=list(probx_cut)]
+
+
+
+plot(pinnacle_p1_1x2_initial_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "blue", pch = 2)
+points(pinnacle_p1_1x2_final_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "purple", pch = 5)
+abline(0,1,col='red')
+
+plot(pinnacle_p2_1x2_initial_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "blue", pch = 2)
+points(pinnacle_p2_1x2_final_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "purple", pch = 5)
+abline(0,1,col='red')
+
 
 plot(pinnacle_pX_1x2_initial_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "blue", pch = 2)
 points(pinnacle_pX_1x2_final_summary[,list(empirical_over,probabilistic_over)],cex=2, col = "purple", pch = 5)
