2524242f2b34de86956fdf9bd626a1fd5c20d451
Alim Buğra Çınar <abugracinar@gmail.com>
2018-10-18 11:46:35 +0300

    Problem is not final data
    Final data requirement is removed but NA problem still continues

--- HW2/HW2_v2.R
+++ HW2/HW2_v2.R
@@ -15,10 +15,11 @@ odds=readRDS(odd_details_file_path)
 unique(odds$bookmaker)
 
 odds_subset <- odds[odds$bookmaker == c("10Bet", "12Bet", "Betway", "Pinnacle", "mybet"),]
-odds_subset[, time := anytime(date)]
-odds_subset[, Year := year(time)]
-odds_subset <- odds_subset[order(matchId, oddtype, bookmaker, date)]
-odds_subset <- odds_subset[,list(final_odd = odd[.N]), by=list(matchId,oddtype,bookmaker)]
+
+#odds_subset[, time := anytime(date)]
+#odds_subset[, Year := year(time)]
+#odds_subset <- odds_subset[order(matchId, oddtype, bookmaker, date)]
+#odds_subset <- odds_subset[,list(final_odd = odd[.N]), by=list(matchId,oddtype,bookmaker)]
 odds_subset <- odds_subset[complete.cases(odds_subset)]
 
 wide_odds <- dcast(odds_subset, matchId ~ bookmaker + oddtype, value.var = "final_odd")
