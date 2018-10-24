88769d10a4c3f6e0b577f1fceb01be6a9f384161
Alim Buğra Çınar <abugracinar@gmail.com>
2018-10-25 01:15:00 +0300

    Shortened the code

--- HW2/HW2.R
+++ HW2/HW2.R
@@ -2,8 +2,10 @@ require(data.table)
 require(anytime)
 require(dplyr)
 library(rgl)
+
 setwd("C:/IE_582_Rep/fall18-bugracnr/HW2")
 getwd()
+
 rm(list=ls())
 gc()
 
@@ -30,203 +32,51 @@ matches[,Is1 := HomeGoals > AwayGoals]
 matches[,Is2 := HomeGoals < AwayGoals]
 matches[,IsX := HomeGoals == AwayGoals]
 matches[,results := Is1*1 + Is2*2 + IsX*3]
+matches <- matches[complete.cases(matches)]
 
-bmakers <- c("10Bet", "bwin", "Expekt", "Pinnacle", "1xBet")
+bmakers <- c("10Bet", "Betfair Exchange", "bet365", "Betclic", "1xBet")
 
 odds <- odds %>% filter(bookmaker %in% bmakers)  %>% mutate()
-str(odds)
 odds <- as.data.table(odds)
-
-odds_ou <- odds[betType == "ou" & totalhandicap == "2.5"]
-
-class(a)
-unique(odds_rest$betType)
-unique(odds[odds$betType == "ha"]$totalhandicap)
-
-odds_rest <- odds[odds$betType != "ou",]
-odds_rest <- odds_rest[odds_rest$betType != "ah",]
-unique(odds_rest$totalhandicap)
-class(odds_rest)
-class(matches)
-odds_ou[order(matchId,oddtype,bookmaker,date)]
-odds_rest[order(matchId,oddtype,bookmaker,date)]
-
-odds_ov_un_initial=odds_ou[,list(final_odd=odd[.N]),
+odds_final=odds[,list(final_odd=odd[.N]),
                               by=list(matchId,oddtype,bookmaker)]
-odds_rest_initial = odds_rest[,list(final_odd=odd[.N]),
-                            by=list(matchId,oddtype,bookmaker)]
-
-
-
-matches <- matches[complete.cases(matches)]
-wide_ov_un_initial <- dcast(odds_ov_un_initial,
-                            matchId ~ bookmaker + oddtype,
-                            value.var="final_odd")
-wide_rest_initial <- dcast(odds_rest_initial,
+wide_final <- dcast(odds_final,
                             matchId ~ bookmaker + oddtype,
                             value.var="final_odd")
 
-odds_ov_un_initial[matchId =="004f4ING"]
-
-wide_all <- merge(wide_ov_un_initial,wide_rest_initial, by = "matchId")
-wide_all <- merge(matches[,c("matchId","IsOver")],wide_all, by.x = "matchId",by.y = "matchId")
-
-
-
-
-pca <- princomp(na.omit(wide_all[2:.N,2:53]), cor = TRUE)
-
-
-
-
-odds_pinnacle <- odds[bookmaker == "Pinnacle"]
-
-
-odds_ou_pinnacle <- odds_pinnacle[betType == "ou" & totalhandicap == "2.5"]
-
-
-odds_rest_pinnacle <- odds_pinnacle[odds_pinnacle$betType != "ou",]
-odds_rest_pinnacle <- odds_rest_pinnacle[odds_rest_pinnacle$betType != "ah",]
-
-odds_ou_pinnacle[order(matchId,oddtype,bookmaker,date)]
-odds_rest_pinnacle[order(matchId,oddtype,bookmaker,date)]
-
-odds_ov_un_initial=odds_ou_pinnacle[,list(final_odd=odd[.N]),
-                           by=list(matchId,oddtype,bookmaker)]
-odds_rest_initial = odds_rest_pinnacle[,list(final_odd=odd[.N]),
-                              by=list(matchId,oddtype,bookmaker)]
-
-
-
-
-matches <- matches[complete.cases(matches)]
-wide_ov_un_initial <- dcast(odds_ov_un_initial,
-                            matchId ~ bookmaker + oddtype,
-                            value.var="final_odd")
-wide_rest_initial <- dcast(odds_rest_initial,
-                           matchId ~ bookmaker + oddtype,
-                           value.var="final_odd")
-
-
-wide_all <- merge(wide_ov_un_initial,wide_rest_initial, by = "matchId")
-wide_all <- merge(matches[,c("matchId","IsOver")],wide_all, by.x = "matchId",by.y = "matchId")
-
-pca <- princomp(na.omit(wide_all[2:.N,2:7]), cor = TRUE)
-
-
-biplot(pca)
+pca <- princomp(na.omit(wide_final[2:.N,2:53]))
 
 summary(pca)
-str(pca)
-plot(pca)
-plot(pca$scores,ylim=c(-4,4),xlim=c(-4,4), col = wide_all$IsOver+2)
 
-head(pca)
-pca$loadings
+plot(pca$scores, col = matches$IsOver + 2)
+plot(pca$scores, col = matches$results + 1)
 
-plot3d(pca$scores[,1:3], col = wide_all$IsOver + 2)
+a <- dist(wide_final[,2:ncol(wide_final)], method = "euclidean")
 
-odds_10Bet <- odds[bookmaker == "Expekt"]
-
-
-odds_ou_10Bet <- odds_10Bet[betType == "ou" & totalhandicap == "2.5"]
-
-
-odds_rest_10Bet <- odds_10Bet[odds_10Bet$betType != "ou",]
-odds_rest_10Bet <- odds_rest_10Bet[odds_rest_10Bet$betType != "ah",]
-
-odds_ou_10Bet[order(matchId,oddtype,bookmaker,date)]
-odds_rest_10Bet[order(matchId,oddtype,bookmaker,date)]
-
-odds_ov_un_initial=odds_ou_10Bet[,list(final_odd=odd[1]),
-                                 by=list(matchId,oddtype,bookmaker)]
-odds_rest_initial = odds_rest_10Bet[,list(final_odd=odd[1]),
-                                    by=list(matchId,oddtype,bookmaker)]
-
-
-
-
-matches <- matches[complete.cases(matches)]
-wide_ov_un_initial <- dcast(odds_ov_un_initial,
-                            matchId ~ bookmaker + oddtype,
-                            value.var="final_odd")
-wide_rest_initial <- dcast(odds_rest_initial,
-                           matchId ~ bookmaker + oddtype,
-                           value.var="final_odd")
-
-
-wide_all <- merge(wide_ov_un_initial,wide_rest_initial, by = "matchId")
-#wide_all <- merge(matches[,c("matchId","IsOver")],wide_all, by.x = "matchId",by.y = "matchId")
-
-pca <- princomp(na.omit(wide_all[2:.N,2:7]), cor = TRUE)
-
-
-biplot(pca)
-
-summary(pca)
-str(pca)
-plot(pca)
-plot(pca$scores,ylim=c(-4,4),xlim=c(-4,4), col = matches$IsOver+2)
-
-head(pca)
-
-
-odds_10Bet <- odds[bookmaker == "bwin"]
-
-
-odds_ou_10Bet <- odds_10Bet[betType == "ou" & totalhandicap == "2.5"]
-
-
-odds_rest_10Bet <- odds_10Bet[odds_10Bet$betType != "ou",]
-odds_rest_10Bet <- odds_rest_10Bet[odds_rest_10Bet$betType != "ah",]
-
-odds_ou_10Bet[order(matchId,oddtype,bookmaker,date)]
-odds_rest_10Bet[order(matchId,oddtype,bookmaker,date)]
-
-odds_ov_un_initial=odds_ou_10Bet[,list(final_odd=odd[.N]),
-                                 by=list(matchId,oddtype,bookmaker)]
-odds_rest_initial = odds_rest_10Bet[,list(final_odd=odd[.N]),
-                                    by=list(matchId,oddtype,bookmaker)]
-
-
-
-
-matches <- matches[complete.cases(matches)]
-wide_ov_un_initial <- dcast(odds_ov_un_initial,
-                            matchId ~ bookmaker + oddtype,
-                            value.var="final_odd")
-wide_rest_initial <- dcast(odds_rest_initial,
-                           matchId ~ bookmaker + oddtype,
-                           value.var="final_odd")
-
-
-wide_all <- merge(wide_ov_un_initial,wide_rest_initial, by = "matchId")
-wide_all <- merge(matches[,c("matchId","IsOver")],wide_all, by.x = "matchId",by.y = "matchId")
-
-pca <- princomp(na.omit(wide_all[2:.N,2:7]))
-
-plot(pca)
-biplot(pca)
-
-summary(pca)
-str(pca)
-plot(pca)
-plot(pca$scores,ylim=c(-4,4),xlim=c(-4,4), col = matches$results)
-
-head(pca)
-
-pca$loadings
-
-
-wide_all
-?dist
-a <- dist(wide_all[,2:ncol(wide_all)], method = "manhattan")
-
-
-distmat[,] <- sqrt(sum())
 
 fit <- cmdscale(a)
 summary(fit)
 fit
 plot(fit[,1],fit[,2],main='Location',xlab='', ylab='',col=1)
+
+
+odds_10Bet <- odds[bookmaker == "10Bet"]
+odds_10Bet_final=odds_10Bet[,list(final_odd=odd[.N]),
+                by=list(matchId,oddtype,bookmaker)]
+wide_10Bet_final <- dcast(odds_10Bet_final,
+                    matchId ~ bookmaker + oddtype,
+                    value.var="final_odd")
+
+pca <- princomp(na.omit(wide_10Bet_final[2:.N,2:ncol(wide_10Bet_final)]))
+plot(pca$scores, col = matches$IsOver + 2)
+plot(pca$scores, col = matches$results + 1)
+
+
+a <- dist(wide_10Bet_final[,2:ncol(wide_10Bet_final)], method = "manhattan")
+
+
+fit <- cmdscale(a)
+summary(fit)
+fit
+plot(fit[,1],fit[,2],main='Location',xlab='', ylab='')
 
--- HW2/HW2_Part3.R
+++ HW2/HW2_Part3.R
@@ -1,6 +1,7 @@
 library(jpeg)
 library(imager)
-
+rm(list=ls())
+gc()
 path <- "C:/IE_582_Rep/fall18-bugracnr/HW2"
 
 setwd(path)
@@ -9,6 +10,8 @@ pic <- readJPEG("HW2_Files/HW2_Part3.jpg")
 class(pic)
 typeof(pic)
 str(pic)
+range(pic[,,2])
+
 
 x <- 1:512
 y <- 1:512
@@ -16,33 +19,42 @@ y <- 1:512
 plot(x,y)
 rasterImage(pic, 1,1,512,512)
 
-r <- pic[,,1]*255
-g <- pic[,,2]*255
-b <- pic[,,3]*255
+r <- t(apply(pic[,,1],2,rev))
+g <- t(apply(pic[,,2],2,rev))
+b <- t(apply(pic[,,3],2,rev))
 
+par(mfrow = c(3,1))
+image(x,y,r, col = rgb(r,0,0))
+image(x,y,g, col = rgb(0,g,0))
+image(x,y,b, col = rgb(0,0,b))
 
-image(x,y,pic[,,1])
-par(new=T)
-image(x,y,pic[,,2])
-par(new=T)
-image(x,y,pic[,,3])
-
-set.seed(15)
+set.seed(1)
 noise_1 <-  matrix(runif(512*512, min = 0, max = 0.1),512)
-set.seed(73)
+set.seed(2)
 noise_2 <-  matrix(runif(512*512, min = 0, max = 0.1),512)
-set.seed(392)
+set.seed(3)
 noise_3 <-  matrix(runif(512*512, min = 0, max = 0.1),512)
 
-pic[,,1] <- pic[,,1] + noise_1
-pic[,,2] <- pic[,,2] + noise_2
-pic[,,3] <- pic[,,3] + noise_3
+noisy_pic <- pic
 
 
-image(x,y,pic[,,1])
-par(new=T)
-image(x,y,pic[,,2])
-par(new=T)
-image(x,y,pic[,,3])
+
+noisy_pic[,,1] <- pic[,,1] + noise_1
+noisy_pic[,,2] <- pic[,,2] + noise_2
+noisy_pic[,,3] <- pic[,,3] + noise_3
+
+noisy_pic <- noisy_pic/max(noisy_pic)
+range(pic)
+range(noise_1)
+
+image(x,y,t(apply(noisy_pic[,,1],2,rev)), col = rgb(r,0,0))
+image(x,y,t(apply(noisy_pic[,,2],2,rev)))
+image(x,y,t(apply(noisy_pic[,,3],2,rev)))
+
+plot(x,y)
 
 
+jpeg("HW2_Files/Noisy_Pic.jpg")
+plot(x,y)
+rasterImage(noisy_pic, 1,1,512,512)
+dev.off()
