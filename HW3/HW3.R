bf6e6ac0d380a3edc8deb0ba4ab208a159206098
Alim Buğra Çınar <abugracinar@gmail.com>
2018-11-08 21:46:47 +0300

    Nearest Neighbor Working

--- HW3/HW3.R
+++ HW3/HW3.R
@@ -1,6 +1,7 @@
 require(plotly)
 require(scatterplot3d)
 
+
 path_xtest <- gsub  ( "\\\\",  "/", "C:/IE_582_Rep/fall18-bugracnr/HW3/HW3_Files/uWaveGestureLibrary_X_TEST"   )
 path_xtrain <- gsub  ( "\\\\",  "/", "C:/IE_582_Rep/fall18-bugracnr/HW3/HW3_Files/uWaveGestureLibrary_X_TRAIN"   )
 path_ytest <- gsub  ( "\\\\",  "/", "C:/IE_582_Rep/fall18-bugracnr/HW3/HW3_Files/uWaveGestureLibrary_Y_TEST"   )
@@ -15,9 +16,15 @@ xtrain <- read.table(path_xtrain)
 ytrain <- read.table(path_ytrain)
 ztrain <- read.table(path_ztrain)
 
+head(xtest)
+
 x <- cumsum(unlist(xtrain[1,2:ncol(xtrain)]))
 y <- cumsum(unlist(ytrain[1,2:ncol(xtrain)]))
 z <- cumsum(unlist(ztrain[1,2:ncol(xtrain)]))
+
+
+
+
 
 scatterplot3d(x,y,z)
 
@@ -28,4 +35,58 @@ z57 <- cumsum(unlist(ztrain[57,2:ncol(xtrain)]))
 
 scatterplot3d(x57,y57,z57)
 
+column_names <- c("x","y","z")
+sample_vector <- rep(NA, 315)
+sample_frame <- data.frame(sample_vector,sample_vector,sample_vector)
+colnames(sample_frame) <- column_names
+all_train <-rep(list(sample_frame), nrow(xtrain))
+names(all_train) <- c(1:nrow(xtrain))
+
+for (i in 1:nrow(xtrain)) {
+  all_train[[i]]$x <- cumsum(unlist(xtrain[i,2:ncol(xtrain)]))
+  all_train[[i]]$y <- cumsum(unlist(ytrain[i,2:ncol(ytrain)]))
+  all_train[[i]]$z <- cumsum(unlist(ztrain[i,2:ncol(ztrain)]))
+}
+
+
+all_test <-rep(list(sample_frame), nrow(xtest))
+names(all_test) <- c(1:nrow(xtest))
+
+for (i in 1:nrow(xtest)) {
+  all_test[[i]]$x <- cumsum(unlist(xtest[i,2:ncol(xtest)]))
+  all_test[[i]]$y <- cumsum(unlist(ytest[i,2:ncol(ytest)]))
+  all_test[[i]]$z <- cumsum(unlist(ztest[i,2:ncol(ztest)]))
+}
+
+par(mfrow = c(1,1))
+scatterplot3d(all_train[[57]]$x,all_train[[57]]$y,all_train[[57]]$z)
+
+trainclass <- xtrain[,1]
+testclass <- xtest[,1]
+
+colnum <- ncol(xtrain)
+
+testdata <- cbind(xtest[,2:colnum],ytest[,2:colnum],ztest[,2:colnum])
+
+traindata <- cbind(xtrain[,2:colnum],ytrain[,2:colnum],ztrain[,2:colnum])
+
+dim(traindata)
+
+#one option to compute Euclidean distance of first time series in test data to training
+#series (inefficient)
+testsubject <- 550
+combined=rbind(testdata[testsubject,],traindata)
+distMatrix=dist(combined, method = "euclidean")
+distMatrix=as.matrix(distMatrix)
+
+
+neighborhood=order(distMatrix[1,]) #first array entry is 1, why?: first entry is the distance between 1st elements which is 0
+
+#1-NN
+neighbor=neighborhood[2]-1 #why minus 1? first element is the test data. we need original index
+prediction=trainclass[neighbor]
+
+par(mfrow=c(2,1)) #2x1 grid
+scatterplot3d(all_test[[testsubject]]$x, all_test[[testsubject]]$y, all_test[[testsubject]]$z,type='l',main='Test Data', color = testclass[testsubject]+2)
+scatterplot3d(all_train[[neighbor]]$x,all_train[[neighbor]]$y,all_train[[neighbor]]$z,type='l',main='Training data with minimum Euclidean distance',color=trainclass[neighbor]+2)
 
