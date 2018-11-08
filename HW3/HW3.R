294ad4c97cb643b248f51f92b78a0a4813398f9d
Alim Buğra Çınar <abugracinar@gmail.com>
2018-11-08 22:22:33 +0300

    Cross Validation not finalized yet!

--- HW3/HW3.R
+++ HW3/HW3.R
@@ -1,5 +1,9 @@
 require(plotly)
 require(scatterplot3d)
+require(FNN)
+require(glmnet)
+require(TunePareto)
+require(data.table)
 
 
 path_xtest <- gsub  ( "\\\\",  "/", "C:/IE_582_Rep/fall18-bugracnr/HW3/HW3_Files/uWaveGestureLibrary_X_TEST"   )
@@ -74,7 +78,7 @@ dim(traindata)
 
 #one option to compute Euclidean distance of first time series in test data to training
 #series (inefficient)
-testsubject <- 550
+testsubject <- 782
 combined=rbind(testdata[testsubject,],traindata)
 distMatrix=dist(combined, method = "euclidean")
 distMatrix=as.matrix(distMatrix)
@@ -86,7 +90,77 @@ neighborhood=order(distMatrix[1,]) #first array entry is 1, why?: first entry is
 neighbor=neighborhood[2]-1 #why minus 1? first element is the test data. we need original index
 prediction=trainclass[neighbor]
 
-par(mfrow=c(2,1)) #2x1 grid
+neighbor2 = neighborhood[3]-1
+neighbor3 = neighborhood[4]-1
+neighbor4 = neighborhood[5]-1
+
+testclass[testsubject]
+trainclass[neighbor]
+trainclass[neighbor2]
+trainclass[neighbor3]
+trainclass[neighbor4]
+
+par(mfrow=c(5,1)) #2x1 grid
 scatterplot3d(all_test[[testsubject]]$x, all_test[[testsubject]]$y, all_test[[testsubject]]$z,type='l',main='Test Data', color = testclass[testsubject]+2)
 scatterplot3d(all_train[[neighbor]]$x,all_train[[neighbor]]$y,all_train[[neighbor]]$z,type='l',main='Training data with minimum Euclidean distance',color=trainclass[neighbor]+2)
+scatterplot3d(all_train[[neighbor2]]$x,all_train[[neighbor2]]$y,all_train[[neighbor2]]$z,type='l',main='Training data with minimum Euclidean distance',color=trainclass[neighbor2]+2)
+scatterplot3d(all_train[[neighbor3]]$x,all_train[[neighbor3]]$y,all_train[[neighbor3]]$z,type='l',main='Training data with minimum Euclidean distance',color=trainclass[neighbor3]+2)
+scatterplot3d(all_train[[neighbor4]]$x,all_train[[neighbor4]]$y,all_train[[neighbor4]]$z,type='l',main='Training data with minimum Euclidean distance',color=trainclass[neighbor4]+2)
 
+
+
+traindata <- as.matrix(read.table(fname))  # read data into a matrix named traindata
+#first column is the class variable
+trainclass=traindata[,1]
+#drop first column
+traindata=traindata[,2:ncol(traindata)]
+
+k_levels=c(1,3,5)
+nofReplications=10
+nFolds=10
+indices=generateCVRuns(trainclass,nofReplications,nFolds,stratified=TRUE)
+
+cvresult=data.table()
+for(i in 1:nofReplications) {
+  thisReplication=indices[[i]]
+  for(j in 1:nFolds){
+    testindices=thisReplication[[j]]
+    
+    cvtrain=traindata[-testindices,]        
+    cvtest=traindata[testindices,]
+    
+    cvtrain=data.frame(Class=trainclass[-testindices],cvtrain)
+   
+    
+    combined=rbind(traindata[testindices,],traindata)
+    distMatrix=dist(combined, method = "euclidean")
+    distMatrix=as.matrix(distMatrix)
+    
+    neighborhood=order(distMatrix[1,]) #first array entry is 1, why?: first entry is the distance between 1st elements which is 0
+    
+    #1-NN
+    neighbor=neighborhood[2]-1 #why minus 1? first element is the test data. we need original index
+    prediction=trainclass[neighbor]
+    
+    
+    if(nrow(cvresult)==0){
+      cvresult=data.table(Replication=i,Fold=j,Method='NN',Klev=NA,TestId=testindices,
+                          Predictions=prediction,Real=trainclass[testindices])
+    } else {
+      cvresult=rbind(cvresult,data.table(Replication=i,Fold=j,Method='NN',Klev=NA,TestId=testindices,
+                                         Predictions=prediction, Real=trainclass[testindices]))
+    }
+    cvtrain=traindata[-testindices,]        
+    cvtest=traindata[testindices,]
+    
+    for(y in 1:length(k_levels)){
+      param_k=k_levels[y]
+      predict_knn=knn(cvtrain, cvtest,trainclass[-testindices], k = param_k)
+      cvresult=rbind(cvresult,data.table(Replication=i,Fold=j,Method='knn',Klev=param_k,TestId=testindices,
+                                         Predictions=as.numeric(as.character(predict_knn)),Real=trainclass[testindices]))
+    }   
+  }    
+}
+
+
+cvresult[,list(Accu=mean(Predictions==Real)),by=list(Method,Klev)]
