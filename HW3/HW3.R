575c93cf37c11725eee3918c56ca787bf6231121
Alim Buğra Çınar <abugracinar@gmail.com>
2018-11-10 17:08:59 +0300

    Task 1 is completed!

--- HW3/HW3.R
+++ HW3/HW3.R
@@ -5,7 +5,10 @@ require(glmnet)
 require(TunePareto)
 require(data.table)
 require(RANN.L1)
+require(dplyr)
 
+rm(list=ls())
+gc()
 
 path_xtest <- gsub  ( "\\\\",  "/", "C:/IE_582_Rep/fall18-bugracnr/HW3/HW3_Files/uWaveGestureLibrary_X_TEST"   )
 path_xtrain <- gsub  ( "\\\\",  "/", "C:/IE_582_Rep/fall18-bugracnr/HW3/HW3_Files/uWaveGestureLibrary_X_TRAIN"   )
@@ -21,48 +24,131 @@ xtrain <- read.table(path_xtrain)
 ytrain <- read.table(path_ytrain)
 ztrain <- read.table(path_ztrain)
 
-x <- cumsum(unlist(xtrain[1,2:ncol(xtrain)]))
-y <- cumsum(unlist(ytrain[1,2:ncol(xtrain)]))
-z <- cumsum(unlist(ztrain[1,2:ncol(xtrain)]))
+cols <- rep("black",315)
+cols[1] <- "blue"
+cols[315] <- "red"
+shapes <- rep(".",315)
+shapes[1] <- 1
+shapes[315] <- 4
 
-scatterplot3d(x,y,z)
-
-
-x57 <- cumsum(unlist(xtrain[57,2:ncol(xtrain)]))
-y57 <- cumsum(unlist(ytrain[57,2:ncol(xtrain)]))
-z57 <- cumsum(unlist(ztrain[57,2:ncol(xtrain)]))
-
-scatterplot3d(x57,y57,z57)
-
-
-#### seems unnecessary start ####
-column_names <- c("x","y","z")
-sample_vector <- rep(NA, 315)
-sample_frame <- data.frame(sample_vector,sample_vector,sample_vector)
-colnames(sample_frame) <- column_names
-all_train <-rep(list(sample_frame), nrow(xtrain))
-names(all_train) <- c(1:nrow(xtrain))
-
-for (i in 1:nrow(xtrain)) {
-  all_train[[i]]$x <- cumsum(unlist(xtrain[i,2:ncol(xtrain)]))
-  all_train[[i]]$y <- cumsum(unlist(ytrain[i,2:ncol(ytrain)]))
-  all_train[[i]]$z <- cumsum(unlist(ztrain[i,2:ncol(ztrain)]))
+pathplotter <- function(ax,ay,az)
+{
+  vx <- cumsum(ax)
+  vy <- cumsum(ay)
+  vz <- cumsum(az)
+  
+  xx <- rep(NA, 315)
+  xx[1] <- 0
+  xy <- rep(NA, 315)
+  xy[1] <- 0
+  xz <- rep(NA, 315)
+  xz[1] <- 0
+  
+  
+  for (i in 2:315) {
+    xx[i] <- (vx[i] - vx[i-1])/2
+    xy[i] <- (vy[i] - vy[i-1])/2
+    xz[i] <- (vz[i] - vz[i-1])/2
+  }
+  
+  xx <- cumsum(xx) + cumsum(vx)
+  xy <- cumsum(xy) + cumsum(vy)
+  xz <- cumsum(xz) + cumsum(vz)
+  
+  scatterplot3d(xz,xy,xx, color = cols, pch = shapes)
 }
 
+############### TASK 1.1
+#class 1 instance
+xtrain[11,1]
 
-all_test <-rep(list(sample_frame), nrow(xtest))
-names(all_test) <- c(1:nrow(xtest))
+ax <- unlist(xtrain[11,2:ncol(xtrain)])
+ay <- unlist(ytrain[11,2:ncol(xtrain)])
+az <- unlist(ztrain[11,2:ncol(xtrain)])
 
-for (i in 1:nrow(xtest)) {
-  all_test[[i]]$x <- cumsum(unlist(xtest[i,2:ncol(xtest)]))
-  all_test[[i]]$y <- cumsum(unlist(ytest[i,2:ncol(ytest)]))
-  all_test[[i]]$z <- cumsum(unlist(ztest[i,2:ncol(ztest)]))
-}
 
-par(mfrow = c(1,1))
-scatterplot3d(all_train[[57]]$x,all_train[[57]]$y,all_train[[57]]$z)
+pathplotter(ax,ay,az)
 
-### seems unnecessary end ###
+
+#class 2 instance
+xtrain[20,1]
+
+ax <- unlist(xtrain[20,2:ncol(xtrain)])
+ay <- unlist(ytrain[20,2:ncol(xtrain)])
+az <- unlist(ztrain[20,2:ncol(xtrain)])
+
+
+pathplotter(ax,ay,az)
+
+
+#class 3 instance
+xtrain[4,1]
+
+ax <- unlist(xtrain[4,2:ncol(xtrain)])
+ay <- unlist(ytrain[4,2:ncol(xtrain)])
+az <- unlist(ztrain[4,2:ncol(xtrain)])
+
+
+pathplotter(ax,ay,az)
+
+#class 4 instance
+xtrain[5,1]
+
+ax <- unlist(xtrain[5,2:ncol(xtrain)])
+ay <- unlist(ytrain[5,2:ncol(xtrain)])
+az <- unlist(ztrain[5,2:ncol(xtrain)])
+
+
+pathplotter(ax,ay,az)
+
+#class 5 instance
+xtrain[2,1]
+
+ax <- unlist(xtrain[2,2:ncol(xtrain)])
+ay <- unlist(ytrain[2,2:ncol(xtrain)])
+az <- unlist(ztrain[2,2:ncol(xtrain)])
+
+
+pathplotter(ax,ay,az)
+
+
+#class 6 instance
+xtrain[1,1]
+
+ax <- unlist(xtrain[1,2:ncol(xtrain)])
+ay <- unlist(ytrain[1,2:ncol(xtrain)])
+az <- unlist(ztrain[1,2:ncol(xtrain)])
+
+
+pathplotter(ax,ay,az)
+
+
+#class 7 instance
+xtrain[7,1]
+
+ax <- unlist(xtrain[7,2:ncol(xtrain)])
+ay <- unlist(ytrain[7,2:ncol(xtrain)])
+az <- unlist(ztrain[7,2:ncol(xtrain)])
+
+
+pathplotter(ax,ay,az)
+
+#class 8 instance
+
+xtrain[6,1]
+
+ax <- unlist(xtrain[6,2:ncol(xtrain)])
+ay <- unlist(ytrain[6,2:ncol(xtrain)])
+az <- unlist(ztrain[6,2:ncol(xtrain)])
+
+
+pathplotter(ax,ay,az)
+
+
+rm(ax,ay,az)
+gc()
+
+############ TASK 1.2
 
 trainclass <- xtrain[,1]
 testclass <- xtest[,1]
@@ -73,12 +159,14 @@ testdata <- cbind(xtest[,2:colnum],ytest[,2:colnum],ztest[,2:colnum])
 
 traindata <- cbind(xtrain[,2:colnum],ytrain[,2:colnum],ztrain[,2:colnum])
 
-
-k_levels=c(1)
+########## Distance Measure : Euclidean
+k_levels=c(1:10)
 nofReplications=10
 nFolds=10
 indices=generateCVRuns(trainclass,nofReplications,nFolds,stratified=TRUE)
 cvresult=data.table()
+
+cv_euc_time <- system.time(
 for(i in 1:nofReplications) {
   thisReplication=indices[[i]]
   for(j in 1:nFolds){
@@ -96,12 +184,16 @@ for(i in 1:nofReplications) {
     }   
   }    
 }
-
+)
 
 cvresult[,list(Accu=mean(Predictions==Real)),by=list(Method,Klev)]
-table(cvresult$Predictions, cvresult$Real)
+##### optimal k is 1 ##########
 
+
+###### Distance Measure: Manhattan
 cvresult=data.table()
+
+cv_manh_time <- system.time(
 for(i in 1:nofReplications) {
     thisReplication=indices[[i]]
   for(j in 1:nFolds){
@@ -122,21 +214,34 @@ for(i in 1:nofReplications) {
     }   
   }    
 }
+)
 
 cvresult[,list(Accu=mean(Predictions==Real)),by=list(Method,Klev)]
-confusion_matrix <- table(cvresult$Predictions, cvresult$Real)
-accuracy <- sum(diag(confusion_matrix))/sum(confusion_matrix)
+##### optimal k is 1 ##########
 
-#backup start 
-combined=rbind(traindata[testindices,],traindata)
-distMatrix=dist(combined, method = "euclidean")
-distMatrix=as.matrix(distMatrix)
+###### End of Task 1.2
 
-neighborhood=order(distMatrix[1,]) #first array entry is 1, why?: first entry is the distance between 1st elements which is 0
+########### Task 1.3
 
-#1-NN
-neighbor <- (neighborhood[2] >  testindices)*neighborhood[2] 
-neighbor <- (neighborhood[2] <= testindices)*(neighborhood[2]-1)
+## Euclidean
+euc_pred_time <- system.time(predict_knn <- knn(traindata,testdata, trainclass, k=1))
+Predictions=as.numeric(as.character(predict_knn))
+euc_comp <- (Predictions==testclass)
+table(Predictions,testclass)
+accuracy <- sum(euc_comp)/length(testclass)
+accuracy
 
-prediction=trainclass[neighbor]
-#backup end
+## Manhattan
+manh_pred_time <- system.time(predict_knn <- nn2(traindata, testdata, k = 1))
+Predictions <- trainclass[as.vector(predict_knn$nn.idx[,1])]
+man_comp <- (Predictions==testclass)
+table(Predictions,testclass)
+accuracy <- sum(man_comp)/length(testclass)
+accuracy
+
+#### System Times
+cv_euc_time
+cv_manh_time
+euc_pred_time
+manh_pred_time
+
